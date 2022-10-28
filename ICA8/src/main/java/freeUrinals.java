357fb3f2b98e18543707836d5938b19618a9c539
Loka Kalyan Balla <lballa@asu.edu>
2022-10-27 17:28:56 -0700

    skeleton code

---
+++ ICA8/src/main/java/freeUrinals.java
@@ -0,0 +1,15 @@
+public class freeUrinals {
+    void getString()
+    {
+
+    }
+    void getfile()
+    {
+
+    }
+    Boolean checkFreeUrinals(String str)
+    {
+        System.out.println("Skeleton code");
+        return true;
+    }
+}
---
+++ ICA8/src/main/java/urinals.java
@@ -0,0 +1,6 @@
+public class urinals {
+    public static void main(String args[])
+    {
+        System.out.println("skeleton code");
+    }
+}
