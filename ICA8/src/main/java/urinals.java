49fcf6e713877a40defdff333772c3749e08bf27
Loka Kalyan Balla <lballa@asu.edu>
2022-10-27 22:40:12 -0700

    added code for validation tests

--- ICA8/src/main/java/urinals.java
+++ ICA8/src/main/java/urinals.java
@@ -1,6 +1,38 @@
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.util.Scanner;
+
 public class urinals {
     public static void main(String args[])
     {
-        System.out.println("skeleton code");
+        freeUrinals f = new freeUrinals();
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        while(true)
+        {
+            System.out.println("Enter your Input:");
+            System.out.println("1-> Input from keyboard\n 2->Read input from File\n 3->Exit" );
+            choice = sc.next();
+            if(choice.equals("1"))
+            {
+                System.out.println("Enter the input string:");
+                String str = sc.next();
+                if(f.validateString(str))
+                {
+                    System.out.println(f.checkFreeUrinals(str));
+                }
+                else{
+                    System.out.println("Please enter correct input");
+                }
+            }
+            if(choice.equals("2"))
+            {
+                System.out.println("nothing");
+            }
+            if(choice.equals("3"))
+            {
+                System.out.println("exiting");
+                break;
+            }
+        }
     }
 }
